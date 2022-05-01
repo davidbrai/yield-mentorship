958eafca017d09ebafb2a4ccf1288d16aa5b85cf
David Brailovsky <davidbrai@gmail.com>
2022-05-01 10:50:14 +0200

    fix: fix build

--- test/SimpleRegistry.t.sol
+++ test/SimpleRegistry.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "forge-std/test.sol";
+import "forge-std/Test.sol";
 import "src/SimpleRegistry.sol";
 
 contract SimpleRegisterTest is Test {

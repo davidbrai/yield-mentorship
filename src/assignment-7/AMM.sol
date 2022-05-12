742008310f47ecd66a38c0041acd816e610aa180
David Brailovsky <davidbrai@gmail.com>
2022-05-12 13:52:17 +0200

    add AMM contract

---
+++ src/assignment-7/AMM.sol
@@ -0,0 +1,226 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {ERC20Permit} from "yield-utils-v2/token/ERC20Permit.sol";
+import {IERC20} from "yield-utils-v2/token/IERC20.sol";
+
+/// @title A constant product automated market maker
+/// @author davidbrai
+/// @notice Facilitates trades between 2 ERC20 tokens by maintaining a constant ratio of their reserves
+///     The AMM itself is also an ERC20 of LP (liquidity provider) tokens, which represent the ownership
+///     of the tokens reserves.
+/// @dev Explain to a developer any extra details
+contract AMM is ERC20Permit {
+
+    /******************
+     * Immutables
+     ******************/
+
+    /// @notice First ERC20 token of this AMM
+    IERC20 public immutable token0;
+
+    /// @notice Second ERC20 token of this AMM
+    IERC20 public immutable token1; 
+
+    /******************
+     * Storage
+     ******************/
+
+    /// @notice Amount of `token0` currently held by the contract
+    uint256 public reserve0;
+
+    /// @notice Amount of `token1` currently held by the contract
+    uint256 public reserve1;
+
+    /// @notice Indicates whether the contract has been initalized with initial reserves
+    bool public initalized;
+
+    /******************
+     * Events
+     ******************/
+
+    /// @notice Event emitted when first initalizing the AMM
+    event Initalized(uint256 amount0, uint256 amount1);
+
+    /// @notice Event emitted when a user deposits tokens into the AMM and gets LP tokens in return
+    event Mint(address indexed user, uint256 amount0, uint256 amount1, uint256 amountLP);
+
+    /// @notice Event emitted when a user burns their LP tokens in return for tokens from the reserve
+    event Burn(address indexed user, uint256 amount0, uint256 amount1, uint256 amountLP);
+
+    /// @notice Event emitted when a user sells `token0` into the AMM in return for `token1`
+    event Sell0(address indexed user, uint256 amount0, uint256 amount1);
+
+    /// @notice Event emitted when a user sells `token1` into the AMM in return for `token0`
+    event Sell1(address indexed user, uint256 amount0, uint256 amount1);
+
+    /******************
+     * Errors
+     ******************/
+
+    error AlreadyInitialized();
+    error IncorrectProportion();
+
+    /// @notice Creates a new AMM
+    /// @param token0_ First ERC20 token address of this AMM
+    /// @param token1_ Second ERC20 token address of this AMM
+    /// @param name The name of the AMM LP tokens
+    /// @param symbol The symbol of the AMM LP tokens
+    /// @param decimals The decimal numbers of the AMM LP tokens
+    constructor(
+        IERC20 token0_,
+        IERC20 token1_,
+        string memory name, 
+        string memory symbol, 
+        uint8 decimals) ERC20Permit(name, symbol, decimals) {
+
+        token0 = token0_;
+        token1 = token1_;
+    }
+
+    /// @notice Deposits the initial token reserves into the AMM
+    ///     The initalize exchange rate of the AMM will be set by the ratio between the token amounts
+    /// @dev This function can only be called once, will revert if called twice
+    /// @param amount0 Amount of `token0` to deposit
+    /// @param amount1 Amount of `token1` to deposit
+    /// @return lpTokens Amount of `lpTokens` minted to the user
+    function init(uint256 amount0, uint256 amount1) public returns (uint256 lpTokens) {
+        if (initalized) {
+            revert AlreadyInitialized();
+        }
+        initalized = true;
+
+        lpTokens = amount0 * amount1;
+
+        reserve0 = amount0;
+        reserve1 = amount1;
+
+        _mint(msg.sender, lpTokens);
+
+        token0.transferFrom(msg.sender, address(this), amount0);
+        token1.transferFrom(msg.sender, address(this), amount1);
+
+        emit Initalized(amount0, amount1);
+    }
+
+    /// @notice Deposits additional tokens into the AMM and mints new LP tokens
+    ///     The ratio between the token amounts must match the current reserve ratio
+    /// @param amount0 Amount of `token0` to deposit
+    /// @param amount1 Amount of `token1` to deposit
+    /// @return lpTokens Amount of `lpTokens` minted to the user
+    function mint(uint256 amount0, uint256 amount1) public returns (uint256 lpTokens) {
+        // Cache variables to reduce SLOADs
+        uint256 reserve0_ = reserve0;
+        uint256 reserve1_ = reserve1;
+
+        // Check proportion:
+        //     reserve0 / reserve1 == amount0 / amount1
+        //     reserve0 * amount1 == reserve1 * amount0
+        if (amount0 * reserve1_ != amount1 * reserve0_) {
+            revert IncorrectProportion();
+        }
+
+        // Mint LP tokens
+        // z = totalSupply * amount0 / reserve0
+        lpTokens = (amount0 * _totalSupply) / reserve0;
+
+        reserve0 = reserve0_ + amount0;
+        reserve1 = reserve1_ + amount1;
+
+        _mint(msg.sender, lpTokens);
+
+        // transfers tokens into AMM
+        token0.transferFrom(msg.sender, address(this), amount0);
+        token1.transferFrom(msg.sender, address(this), amount1);
+
+        emit Mint(msg.sender, amount0, amount1, lpTokens);
+    }
+
+    /// @notice Burns LP tokens and gets back reserve tokens
+    /// @param lpTokens Amount of LP tokens to burn
+    /// @return amount0 Amount of `token0` sent to the user
+    /// @return amount1 Amount of `token1` sent to the user
+    function burn(uint256 lpTokens) public returns (uint256 amount0, uint256 amount1) {
+        // Cache variables to reduce SLOADs
+        uint256 reserve0_ = reserve0;
+        uint256 reserve1_ = reserve1;
+
+        // calculate amounts
+        amount0 = (reserve0_ * lpTokens) / _totalSupply;
+        amount1 = (reserve1_ * lpTokens) / _totalSupply;
+
+        // updates reserves
+        reserve0 = reserve0_ - amount0;
+        reserve1 = reserve1_ - amount1;
+        
+        // burn
+        _burn(msg.sender, lpTokens);
+
+        // send underlying to user
+        token0.transfer(msg.sender, amount0);
+        token1.transfer(msg.sender, amount1);
+
+        emit Burn(msg.sender, amount0, amount1, lpTokens);
+    }
+
+    /// @notice Sells a specified amount of `token0` and gets some `token1`
+    ///     while maintaining the same product of reserve tokens
+    /// @dev In order the maintain constant product, `amount1 is calculated as follows:
+    ///     notation:
+    ///       x0, y0 = reserve0, reserve1
+    ///       x, y = amount0, amount1
+    ///       k = x0 * y0
+    ///     constant product requirement:
+    ///       k = (x0 + x) * (y0 - y)
+    ///     solve for y:
+    ///       x0*y0 - x0*y + x*y0 - x*y = k
+    ///       k - x0*y + x*y0 - x*y = k
+    ///       - x0*y + x*y0 - x*y = 0
+    ///       x*y0 = x0*y + x*y = y * (x0+x)
+    ///       y = (x * y0) / (x0 + x)
+    /// @param amount0 Amount of `token0` to sell into the AMM
+    /// @return amount1 Amount of `token1` received back from the AMM
+    function sell0(uint256 amount0) public returns (uint256 amount1) {
+        // Cache variables to reduce SLOADs
+        uint256 reserve0_ = reserve0;
+        uint256 reserve1_ = reserve1;
+        
+        amount1 = (amount0 * reserve1_) / (reserve0_ + amount0);
+
+        // update reserve
+        reserve0 = reserve0_ + amount0;
+        reserve1 = reserve1_ - amount1;
+
+        // send amount0 to amm
+        token0.transferFrom(msg.sender, address(this), amount0);
+
+        // send amount1 to user
+        token1.transfer(msg.sender, amount1);
+
+        emit Sell0(msg.sender, amount0, amount1);
+    }
+
+    /// @notice Sells a specified amount of `token1` and gets some `token0`
+    ///     while maintaining the same product of reserve tokens
+    /// @dev Same calculation as in sell0 but swap the tokens
+    /// @param amount1 Amount of `token1` to sell into the AMM
+    /// @return amount0 Amount of `token0` received back from the AMM
+    function sell1(uint256 amount1) public returns (uint256 amount0) {
+        uint256 reserve0_ = reserve0;
+        uint256 reserve1_ = reserve1;
+
+        amount0 = (amount1 * reserve0_) / (reserve1_ + amount1);
+
+        // update reserve
+        reserve0 = reserve0_ - amount0;
+        reserve1 = reserve1_ + amount1;
+
+        // send amount1 to amm
+        token1.transferFrom(msg.sender, address(this), amount1);
+
+        // send amount0 to user
+        token0.transfer(msg.sender, amount0);
+
+        emit Sell1(msg.sender, amount0, amount1);
+    }
+}
---
+++ test/assignment-7/AMM.t.sol
@@ -0,0 +1,194 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import {AMM} from "src/assignment-7/AMM.sol";
+import {ERC20Mock} from "yield-utils-v2/mocks/ERC20Mock.sol";
+
+abstract contract ZeroState is Test {
+
+    AMM amm;
+    ERC20Mock token0;
+    ERC20Mock token1;
+    address alice = address(1);
+    address bob = address(2);
+
+    function setUp() public virtual {
+        vm.label(alice, "alice");
+        vm.label(bob, "bob");
+
+        token0 = new ERC20Mock("token0", "T0");
+        token1 = new ERC20Mock("token1", "T1");
+
+        vm.label(address(token0), "token0");
+        vm.label(address(token1), "token1");
+
+        amm = new AMM(token0, token1, "dex", "LP", 18);
+        vm.label(address(amm), "AMM");
+
+        vm.startPrank(alice);
+        token0.approve(address(amm), type(uint256).max);
+        token1.approve(address(amm), type(uint256).max);
+        vm.stopPrank();
+
+        vm.startPrank(bob);
+        token0.approve(address(amm), type(uint256).max);
+        token1.approve(address(amm), type(uint256).max);
+        vm.stopPrank();
+    }
+
+    function assertRelApproxEq(
+        uint256 a,
+        uint256 b,
+        uint256 maxPercentDelta // An 18 decimal fixed point number, where 1e18 == 100%
+    ) internal virtual {
+        if (b == 0) return assertEq(a, b); // If the expected is 0, actual must be too.
+
+        uint256 percentDelta = ((a > b ? a - b : b - a) * 1e18) / b;
+
+        if (percentDelta > maxPercentDelta) {
+            emit log("Error: a ~= b not satisfied [uint]");
+            emit log_named_uint("    Expected", b);
+            emit log_named_uint("      Actual", a);
+            emit log_named_decimal_uint(" Max % Delta", maxPercentDelta, 18);
+            emit log_named_decimal_uint("     % Delta", percentDelta, 18);
+            fail();
+        }
+    }
+}
+
+contract ZeroStateTest is ZeroState {
+
+    event Initalized(uint256 amount0, uint256 amount1);
+
+    function testInit() public {
+        token0.mint(alice, 5 ether);
+        token1.mint(alice, 10 ether);
+
+        vm.prank(alice);
+        vm.expectEmit(true, true, true, true);
+        emit Initalized(5 ether, 10 ether);
+        uint z = amm.init(5 ether, 10 ether);
+
+        assertEq(z, 50 * 1e36);
+        assertEq(amm.balanceOf(alice), 50 * 1e36);
+    }
+}
+
+abstract contract InitializedState is ZeroState {
+    function setUp() public override virtual {
+        super.setUp();
+
+        token0.mint(alice, 99999 ether);
+        token1.mint(alice, 99999 ether);
+
+        vm.prank(alice);
+        amm.init(5 ether, 10 ether);
+    }
+}
+
+contract InitializedStateTest is InitializedState {
+
+    event Mint(address indexed user, uint256 amount0, uint256 amount1, uint256 amountLP);
+    event Burn(address indexed user, uint256 amount0, uint256 amount1, uint256 amountLP);
+    event Sell0(address indexed user, uint256 amount0, uint256 amount1);
+    event Sell1(address indexed user, uint256 amount0, uint256 amount1);
+
+    function testRevertsIfCallingInitTwice() public {
+        token0.mint(alice, 1 ether);
+        token1.mint(alice, 1 ether);
+
+        vm.prank(alice);
+        vm.expectRevert(AMM.AlreadyInitialized.selector);
+        amm.init(1 ether, 1 ether);
+    }
+
+    function testMintSameAmountShouldMintSameAmountOfLPTokens() public {
+        vm.prank(alice);
+        vm.expectEmit(true, true, true, true);
+        emit Mint(alice, 5 ether, 10 ether, 50 * 1e36);
+        uint z = amm.mint(5 ether, 10 ether);
+
+        assertEq(z, 50 * 1e36);
+        assertEq(amm.balanceOf(alice), 100 * 1e36);
+    }
+
+    function testMintDifferentAmount() public {
+        vm.prank(alice);
+        uint z = amm.mint(0.1234 ether, 0.2468 ether);
+
+        assertEq(z, (0.1234/5) * 50 * 1e36);
+    }
+
+    function testMintRevertsIfWrongProportion() public {
+        vm.prank(alice);
+        vm.expectRevert(AMM.IncorrectProportion.selector);
+        amm.mint(5 ether, 9 ether);
+    }
+
+    function testBurn() public {
+        // Alice current LP token balance
+        assertEq(amm.balanceOf(alice), 50 * 1e36);
+
+        // Burn all alice tokens for easy calculation
+        token0.burn(alice, token0.balanceOf(alice));
+        token1.burn(alice, token1.balanceOf(alice));
+
+        vm.prank(alice);
+        vm.expectEmit(true, true, true, true);
+        emit Burn(alice, 1 ether, 2 ether, 10 * 1e36);
+        (uint x, uint y) = amm.burn(10 * 1e36);
+
+        assertEq(x, 1 ether);
+        assertEq(y, 2 ether);
+
+        assertEq(token0.balanceOf(alice), 1 ether);
+        assertEq(token1.balanceOf(alice), 2 ether);
+    }
+
+    function testSell0() public {
+        token0.mint(bob, 10 ether);
+        uint256 k = amm.reserve0() * amm.reserve1();
+
+        // expected swap amount: (1*10)/(5+1) = 1.6666...
+        vm.prank(bob);
+        vm.expectEmit(true, true, true, true);
+        emit Sell0(bob, 1 ether, uint(5) * 1e18 / 3);
+        uint amount1 = amm.sell0(1 ether);
+        
+        assertEq(amount1, uint(5) * 1e18 / 3);
+        assertEq(token1.balanceOf(bob), amount1);
+
+        // constant product is maintained
+        assertRelApproxEq(amm.reserve0() * amm.reserve1(), k, 0);
+
+        vm.prank(bob);
+        uint256 amount1_2 = amm.sell0(1 ether);
+        
+        assertLt(amount1_2, amount1);
+        assertEq(token1.balanceOf(bob), amount1 + amount1_2);
+    }
+
+    function testSell1() public {
+        token1.mint(bob, 10 ether);
+        uint256 k = amm.reserve0() * amm.reserve1();
+
+        // expected swap amount: (1*5)/(10+1) = 0.45454545...
+        vm.prank(bob);
+        vm.expectEmit(true, true, true, true);
+        emit Sell1(bob, uint(5) * 1e18 / 11, 1 ether);
+        uint amount0 = amm.sell1(1 ether);
+        
+        assertEq(amount0, uint(5) * 1e18 / 11);
+        assertEq(token0.balanceOf(bob), amount0);
+
+        // constant product is maintained
+        assertRelApproxEq(amm.reserve0() * amm.reserve1(), k, 0);
+
+        vm.prank(bob);
+        uint256 amount0_2 = amm.sell1(1 ether);
+        
+        assertLt(amount0_2, amount0);
+        assertEq(token0.balanceOf(bob), amount0 + amount0_2);
+    }
+}
